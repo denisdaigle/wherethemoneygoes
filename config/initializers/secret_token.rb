7e74b4b579410a0a9f56aed5b94e514ec964af86
Denis Daigle <dadaigle@gmail.com>
2012-09-21 07:50:59 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wherethemoneygoes::Application.config.secret_token = '0b667144944ee78fa5aa46f0bc51f682611164295c1b25da26129b1016a91745380ba5d5270971fe549486612f608de29b98478a1645adf19df672a9dfa61382'
